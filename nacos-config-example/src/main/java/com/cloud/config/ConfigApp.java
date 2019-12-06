bb0056be3b8f1f60d31ff19820b1e82654b3dbc6
wcy <s2288156@163.com>
2019-12-06 10:39:46 +0800

    nacos config多配置文件

--- nacos-config-example/src/main/java/com/cloud/config/ConfigApp.java
+++ nacos-config-example/src/main/java/com/cloud/config/ConfigApp.java
@@ -1,16 +1,37 @@
 package com.cloud.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author wcy
  */
+@RefreshScope
+@RestController
 @SpringBootApplication
 public class ConfigApp {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApp.class, args);
     }
 
+    @Value("${refresh}")
+    private String refresh;
+
+    @Value("${user}")
+    private String user;
+
+    @GetMapping("/refresh")
+    public String refresh() {
+        return refresh;
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return user;
+    }
 
 }
