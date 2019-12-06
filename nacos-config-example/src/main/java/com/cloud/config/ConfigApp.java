731b3d22ff5b88576efba0fe9890972db88205ee
wcy <s2288156@163.com>
2019-12-06 09:54:27 +0800

    import nacos config

---
+++ nacos-config-example/src/main/java/com/cloud/config/ConfigApp.java
@@ -0,0 +1,16 @@
+package com.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wcy
+ */
+@SpringBootApplication
+public class ConfigApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApp.class, args);
+    }
+
+
+}
